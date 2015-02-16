995fa6775775f39ef5c2fa19075f6a7be989592f
Kemyke <kemyyy@gmail.com>
2015-02-16 21:51:11 +0100

    Demo Init

---
+++ PalinkaAppDemo/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using PalinkaAppDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PalinkaAppDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dilution()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ComputeDilution(DilutionViewModel vm)
+        {
+            var x = (vm.OriginalAmount * vm.OriginalConcentration) / vm.RequiredConcentration - vm.OriginalAmount;
+            
+            ModelState.SetModelValue("AdditionalAmount", new ValueProviderResult(x, "", CultureInfo.CurrentCulture));
+            return View("Dilution");
+        }
+    }
+}
---
+++ PalinkaAppDemo/Models/DilutionViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PalinkaAppDemo.Models
+{
+    public class DilutionViewModel
+    {
+        [Required]
+        [Display(Name = "Eredeti mennyiség")]
+        public decimal OriginalAmount { get; set; }
+
+        [Required]
+        [Display(Name = "Eredeti szeszfok")]
+        public decimal OriginalConcentration { get; set; }
+        
+        [Required]
+        [Display(Name = "Elvárt szeszfok")]
+        public decimal RequiredConcentration { get; set; }
+
+        [Display(Name = "Hozzáadandó mennyiség")]
+        public decimal? AdditionalAmount { get; set; }
+
+    }
+}
